4faafc60caecd81280d262bdd01e95fa8a6c27b4
chunyukuo88 <alexgochenour@gmail.com>
2025-02-26 17:10:28 -0500

    feat: Redoing this commit
    - Previous commit did not include the relevant modules

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 import {initialGreeting} from "./messaging/initialGreeting.js";
 import messages from "./messaging/messages.js";
+import {queryUserForUrl} from "./messaging/queryUserForUrl.js";
 
 export async function main() {
-  initialGreeting(messages);
+  initialGreeting(messages.initialGreeting);
+  await queryUserForUrl(messages.queryUserForUrl);
 }
 
 await main();
