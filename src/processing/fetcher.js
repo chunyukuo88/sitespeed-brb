3d6da930a1aa4e424a45116e2b1320e302b73065
chunyukuo88 <alexgochenour@gmail.com>
2025-02-27 01:49:36 -0500

    WIP - Figured out the solution to the headers issue.
    - Review this; there might still be rate limits.

--- src/processing/fetcher.js
+++ src/processing/fetcher.js
@@ -1,7 +1,12 @@
 import fetch from 'node-fetch';
 
+const spoofedCurlHeaders = {
+  'User-Agent': 'curl/7.68.0',
+  'Accept': '*/*'
+};
+
 export default async function fetcher(url){
-  const response = await fetch(url);
+  const response = await fetch(url, {headers: spoofedCurlHeaders});
   const asText = await response.text();
   console.log(asText);
 }
