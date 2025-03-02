8ba86967f91f4fc4b2fdaf7759bdc74cc15c2597
chunyukuo88 <alexgochenour@gmail.com>
2025-03-02 13:50:04 -0500

    test: for when Google API has a problem.

--- src/processing/processUrl.spec.js
+++ src/processing/processUrl.spec.js
@@ -3,23 +3,34 @@ import { processUrl } from './processUrl';
 
 jest.mock('./fetcher.js', () => jest.fn());
 
+afterEach(() => jest.clearAllMocks());
+
 describe('processUrl()', () => {
   describe('Validating when the user has NOT entered a valid URL', () => {
-    describe('WHEN: they just pressed "ENTER" too early,', () => {
-      test('THEN: it exits the program with an error.', () => {
-        //
+    describe('WHEN: they just pressed `ENTER` too early,', () => {
+      test('THEN: it exits the program with an error.', async () => {
+        const errorMessage = 'Google API failure';
+        fetcher.mockRejectedValue(new Error(errorMessage));
+        const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
+        const consoleCountSpy = jest.spyOn(console, 'count').mockImplementation(() => {});
+        const urlString = 'www.google.com';
+
+        await processUrl(urlString);
+
+        expect(consoleCountSpy).toHaveBeenCalledTimes(2);
+        expect(consoleErrorSpy).toHaveBeenCalledTimes(1);
       });
     });
     describe('WHEN: the URL itself is not correctly written,', () => {
       test('THEN: it exits the program with an error.', () => {
-        //
+        // TODO: Next card.
       });
     });
   });
   describe('GIVEN: the user has entered a valid URL', () => {
     describe('WHEN: the function executes,', () => {
       test('THEN: it queries the Lighthouse API with the given URL.', async () => {
-        fetcher.mockImplementationOnce(jest.fn())
+        fetcher.mockImplementationOnce(jest.fn());
         const urlString = 'www.google.com';
 
         await processUrl(urlString);
