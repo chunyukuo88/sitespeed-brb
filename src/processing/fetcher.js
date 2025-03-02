8772bc2378ff231d38c4872d4da11a75475c7768
chunyukuo88 <alexgochenour@gmail.com>
2025-03-02 13:39:31 -0500

    refactor: processUrl and fetcher
    - No more need for spoofing.
    - Extracted url builder

--- src/processing/fetcher.js
+++ src/processing/fetcher.js
@@ -1,14 +1,8 @@
 import fetch from 'node-fetch';
 
-// const spoofedCurlHeaders = {
-//   'User-Agent': 'curl/7.68.0',
-//   'Accept': '*/*'
-// };
-
 export default async function fetcher(url){
   console.count('fetcher()');
   try {
-    // const response = await fetch(url, {headers: spoofedCurlHeaders});
     const response = await fetch(url);
     const asJson = await response.json();
     return asJson;
--- src/processing/processUrl.js
+++ src/processing/processUrl.js
@@ -1,12 +1,16 @@
 import fetcher from './fetcher.js';
 import 'dotenv/config';
 
+const buildCompleteUrl = (urlFromUser) => {
+  const key = process.env.GOOGLE_API_KEY;
+  return `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${urlFromUser}&strategy=mobile&key=${key}`;
+};
+
 export async function processUrl(urlString){
   console.count('processUrl()');
   try {
-    const apiKey = process.env.GOOGLE_API_KEY;
-    const command = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${urlString}&strategy=mobile&key=${apiKey}`;
-    const responseAsJson = await fetcher(command);
+    const url = buildCompleteUrl(urlString);
+    const responseAsJson = await fetcher(url);
     return responseAsJson;
   } catch (error) {
     console.count('processUrl() - There was an  error:');
